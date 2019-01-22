ed3ec1e9e0431a95a6d56450815d38e97a7d75ff
adrianocwb <firion_@msn.com>
2019-01-22 21:54:58 -0200

    formato/data/hora

--- js/eventos.js
+++ js/eventos.js
@@ -5,11 +5,18 @@ $.getJSON("model/eventos_listar.php", function (dados) {
     //para cada item em dados gera um valor
     dados.forEach(function (valor) {
 
+        var data = valor.datahora.split(" ");
+        var dia = data[0].split("-");
+        var hora = data[1].split(":");
+
+        var dataFormatado = dia[2]+"/"+dia[1]+"/"+dia[0];
+        var horaFormatado = hora[0]+":"+hora[2];
+
         //gerando a linha com os dados
         var tr = "<tr>"
             + "<td>" + valor.id + "</td>"
             + "<td>" + valor.nome + "</td>"
-            + "<td>" + valor.datahora + "</td>"
+            + "<td>" + dataFormatado +", "+ horaFormatado+ "</td>"
             + "<td>" + valor.local + "</td>"
             + "<td>[Editar][Deletar]</td>"
             + "</tr>";
